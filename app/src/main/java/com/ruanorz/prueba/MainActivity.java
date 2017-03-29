8205fb863125afb342092ce8e23229f76b000833
ruanorz <ruano@outlook.com>
2017-03-29 20:37:21 +0200

    animation shared views

--- app/src/main/java/com/ruanorz/prueba/MainActivity.java
+++ app/src/main/java/com/ruanorz/prueba/MainActivity.java
@@ -1,13 +1,44 @@
 package com.ruanorz.prueba;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageView sharedImage = (ImageView) findViewById(R.id.sharedimage);
+        sharedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This is where the magic happens.
+                // makeSceneTransitionAnimation takes a context, view,
+                // a name for the target view.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Log.e(TAG, "LOLLIPOP or more");
+                    ActivityOptions options =
+                            ActivityOptions.
+                                    makeSceneTransitionAnimation(MainActivity.this, sharedImage, "sharedImage");
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent, options.toBundle());
+                }else{
+                    Log.e(TAG, "Before LOLLIPOP");
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/ruanorz/prueba/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.ruanorz.prueba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ruano on 29/03/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}

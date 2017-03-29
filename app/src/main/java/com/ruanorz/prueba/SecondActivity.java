8b15745c0571d2d2d70b175fad68a3201a678d03
ruanorz <ruano@outlook.com>
2017-03-29 21:06:55 +0200

    animation shared view back

--- app/src/main/java/com/ruanorz/prueba/SecondActivity.java
+++ app/src/main/java/com/ruanorz/prueba/SecondActivity.java
@@ -1,7 +1,13 @@
 package com.ruanorz.prueba;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by ruano on 29/03/2017.
@@ -9,9 +15,33 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String TAG = SecondActivity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        final ImageView sharedImage = (ImageView) findViewById(R.id.sharedimage);
+        sharedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This is where the magic happens.
+                // makeSceneTransitionAnimation takes a context, view,
+                // a name for the target view.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Log.e(TAG, "LOLLIPOP or more");
+                    ActivityOptions options =
+                            ActivityOptions.
+                                    makeSceneTransitionAnimation(SecondActivity.this, sharedImage, "sharedImage");
+                    Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                    startActivity(intent, options.toBundle());
+                }else{
+                    Log.e(TAG, "Before LOLLIPOP");
+                    Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
